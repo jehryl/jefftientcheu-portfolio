a76d27d3d07d164139d584cf7e61dcb3c13b710c
Jeff Tientcheu <jeffdarhyl.tientcheu@epfedu.fr>
2025-08-11 11:40:31 +0200

    Update the config for the deployment on Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+//import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+const isVercel = process.env.VERCEL === '1';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jehryl.github.io',
-  base: 'jefftientcheu-portfolio',
-  trailingSlash: 'always',
+  integrations: [react()],
+  // site: 'https://jehryl.github.io',
+  // base: 'jefftientcheu-portfolio',
+  // trailingSlash: 'always',
 
-  vite: {
-      plugins: [tailwindcss()],
-  },
+  site: isVercel
+    ? "https://jefftientcheu-portfolio.vercel.app/"
+    : "https://jehryl.github.io",
+  base: isVercel ? "/" : "/jefftientcheu-portfolio/",
 
-  integrations: [react()]
+  // vite: {
+      // plugins: [tailwindcss()],
+  // },
 });
